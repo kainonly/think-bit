e71d460d9d930fc6648f8049bcb87b5dd56f7c74
kain <zhangtqx@vip.qq.com>
2019-03-05 10:25:11 +0800

    Update:Traits AddModel

--- src/traits/AddModel.php
+++ src/traits/AddModel.php
@@ -8,7 +8,7 @@ use think\Db;
  * Trait AddModel
  * @package think\bit\traits
  * @property string model 模型名称
- * @property array post POST请求
+ * @property array post 请求主体
  * @property array add_before_result 前置返回结果
  * @property array add_after_result 后置返回结果
  * @property array add_fail_result 新增执行失败结果
@@ -17,59 +17,45 @@ trait AddModel
 {
     public function add()
     {
-        // 通用验证
         $validate = validate($this->model);
         if (!$validate->scene('add')->check($this->post)) return [
             'error' => 1,
             'msg' => $validate->getError()
         ];
 
-        // 判断是否有前置处理
         $this->post['create_time'] = $this->post['update_time'] = time();
-        if (method_exists($this, '__addBeforeHooks')) {
-            $before_result = $this->__addBeforeHooks();
-            if (!$before_result) return $this->add_before_result;
+
+        // 检测是否有前置处理
+        if (method_exists($this, '__addBeforeHooks') &&
+            !$this->__addBeforeHooks()) {
+            return $this->add_before_result;
         }
 
-        // 执行事务写入
-        $transaction = Db::transaction(function () {
-            // 判断是否有后置处理
+        return !Db::transaction(function () {
+            // 检测是否有后置处理
             if (!method_exists($this, '__addAfterHooks')) {
                 return Db::name($this->model)->insert($this->post);
-            } else {
-                // 已存在主键id
-                if (isset($this->post['id']) && !empty($this->post['id'])) {
-                    $result_id = $this->post['id'];
-                    $result = Db::name($this->model)->insert($this->post);
-
-                    if (!$result) {
-                        Db::rollback();
-                        return false;
-                    }
-                } else {
-                    $result_id = Db::name($this->model)->insertGetId($this->post);
-                }
-
-                if (!$result_id) {
-                    Db::rollback();
-                    return false;
-                }
-
-                $after_result = $this->__addAfterHooks($result_id);
-                if (!$after_result) {
-                    $this->add_fail_result = $this->add_after_result;
-                    Db::rollback();
-                    return false;
-                }
-
-                return true;
             }
-        });
-        if ($transaction) return [
+
+            $id = null;
+            if (isset($this->post['id'])) {
+                $id = $this->post['id'];
+                $result = Db::name($this->model)->insert($this->post);
+                if (!$result) return false;
+            } else {
+                $id = Db::name($this->model)->insertGetId($this->post);
+            }
+
+            if (empty($id) || !$this->__addAfterHooks($id)) {
+                $this->add_fail_result = $this->add_after_result;
+                Db::rollback();
+                return false;
+            }
+
+            return true;
+        }) ? $this->add_fail_result : [
             'error' => 0,
             'msg' => 'ok'
-        ]; else {
-            return $this->add_fail_result;
-        }
+        ];
     }
 }
