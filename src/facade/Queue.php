3965c154000d138eea46d2e4b273f94772c42bc5
kain <zhangtqx@vip.qq.com>
2019-04-23 10:34:46 +0800

    更新 PHP DOC

--- src/facade/Queue.php
+++ src/facade/Queue.php
@@ -7,7 +7,7 @@ use think\Facade;
 
 /**
  * Class Collect
- * @method static void push(string $motivation, array $data = [], array $time_field = []) 信息收集推送
+ * @method static void push(string $namespace, array $data = []) 信息收集推送
  * @package bit\facade
  */
 final class Queue extends Facade
