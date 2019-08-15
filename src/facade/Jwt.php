19d18b00f6dece57bd8591b7c2d7127e3b413609
kain <zhangtqx@vip.qq.com>
2019-08-15 22:48:07 +0800

    Update

--- src/facade/Jwt.php
+++ src/facade/Jwt.php
@@ -8,8 +8,8 @@ use think\Facade;
 /**
  * Class Jwt
  * @method static Token getToken()
- * @method static boolean|string setToken(string $scene, array $symbol = [])
- * @method static boolean|string verify(string $scene, string $token)
+ * @method static bool|string setToken(string $scene, array $symbol = [])
+ * @method static bool|string verify(string $scene, string $token)
  * @package think\bit\facade
  */
 final class Jwt extends Facade
